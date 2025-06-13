985985aa073de7af835411e640e5d3927876df40
OverLoad <SwartzMss@users.noreply.github.com>
2025-06-13 13:42:10 +0800

    Remove unused reminder CRUD functions

--- src/configmanager.h
+++ src/configmanager.h
@@ -22,9 +22,6 @@ public:
     void setPaused(bool paused);
     QJsonArray getReminders() const;
     void setReminders(const QJsonArray &reminders);
-    void addReminder(const QJsonObject &reminder);
-    void updateReminder(const QString &id, const QJsonObject &reminder);
-    void deleteReminder(const QString &id);
 
     void saveConfig();
     void loadConfig();
