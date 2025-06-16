4339e71f5e4f8c2424c04037d9e962e16f38994d
OverLoad <SwartzMss@users.noreply.github.com>
2025-06-16 13:04:11 +0800

    Remove unused actions from CompletedReminderList

--- src/completed_reminderlist.h
+++ src/completed_reminderlist.h
@@ -5,7 +5,6 @@
 #include <QSortFilterProxyModel>
 #include <QJsonObject>
 #include <QJsonArray>
-#include "completed_reminderedit.h"
 #include "remindermanager.h"
 #include "completed_remindertablemodel.h"
 #include <QPushButton>
@@ -25,38 +24,24 @@ public:
 
     void setReminderManager(ReminderManager *manager);
     void loadReminders(const QList<Reminder> &reminders);
-    QJsonArray getReminders() const;
-    QPushButton *addButton() const;
     QPushButton *deleteButton() const;
-    QPushButton *importButton() const;
-    QPushButton *exportButton() const;
     QTableView *tableView() const;
 
 public slots:
-    void onAddClicked();
-    void onEditClicked();
     void onDeleteClicked();
-    void onImportClicked();
-    void onExportClicked();
     void onSearchTextChanged(const QString &text);
 
 private:
     void setupConnections();
     void setupModel();
-    void addNewReminder();
-    void editReminder(const QModelIndex &index);
     void deleteReminder(const QModelIndex &index);
     void refreshList();
     void searchReminders(const QString &text);
-    QJsonObject getReminderData(const QString &name) const;
-    void addReminderToModel(const Reminder &reminder);
-    void updateReminderInModel(const Reminder &reminder);
 
     Ui::CompletedReminderList *ui;
     ReminderManager *reminderManager;
     CompletedReminderTableModel *model;
     QSortFilterProxyModel *proxyModel;
-    CompletedReminderEdit *editDialog;
     QString m_searchText;
 };
 
